3ecbae50f8caa1401f89a33de23788854ffe68ef
poulch74 <poulch@mail.ru>
2019-08-09 18:48:05 +0300

    events

--- CODE/shapesp/event.h
+++ CODE/shapesp/event.h
@@ -1,17 +1,17 @@
-#define DEFINE_EVENT(id,num) const int id = num; EspEvent __evt##id(id,EVT_TASK);
+#define DEFINE_EVENT(id,num) const int id = num; EspEventPtr __evt##id(id,EVT_TASK);
 
 #define EVENT_BEGIN_REGISTER_TASKS void EventRegisterTasks() {
 #define EVENT_REGISTER_TASK(id,task,enable) if(enable) { __evt##id.attach((EspTask *)&task); }
 #define EVENT_END_REGISTER_TASKS }
 
-#define DEFINE_MSG(id,num) const int id = num; EspEvent __msg##id(id,EVT_MSG);
+#define DEFINE_MSG(id,num) const int id = num; EspEventPtr __msg##id(id,EVT_MSG);
 
 #define MSG_BEGIN_REGISTER_TASKS void MsgRegisterTasks() {
 #define MSG_REGISTER_TASK(id,task,enable) if(enable) { __msg##id.attach((EspTask *)&task); }
 #define MSG_END_REGISTER_TASKS }
 
 #define MSG_BEGIN_SUBSCRIBE void MsgSubscribe() {
-#define MSG_SUBSCRIBE(msg,id) msglist[msg] = (EspEvent *)&__msg##id;
+#define MSG_SUBSCRIBE(msg,id) msglist[msg] = (EspEventPtr *)&__msg##id;
 #define MSG_END_SUBSCRIBE }
 
 #define GetEvent(id) __evt##id
@@ -24,22 +24,32 @@ class EspTask
 {
 public:
    EspTask(){}
-   virtual void doTask(int evt) {}
+   virtual void doTask(int evt, void *data) {}
    virtual void doWStask(int evt, JsonObject &iroot, JsonObject &root) {}
    virtual void doMqttTask(int evt, std::vector<String> &payload) {}
 };
 
-class EspEvent
+class EspEventPtr
 {
    public:
-      EspEvent(int id, int type) { _id = id; _type=type; }
+      EspEventPtr(int id, int type) { _id = id; _type=type; }
       void attach(EspTask *task) { _tasks.push_back(task); }
-      void doTasks() { for(uint32_t i = 0; i< _tasks.size();i++) _tasks[i]->doTask(_id); }
-      void doTasks(std::vector<String> &payload) { for(uint32_t i = 0; i< _tasks.size();i++) { _tasks[i]->doMqttTask(_id, payload); } }
-      void doTasks(JsonObject& iroot, JsonObject& root) { for(uint32_t i = 0; i< _tasks.size();i++) _tasks[i]->doWStask(_id, iroot, root); }
+
+      void doTasks(void *data) { for(auto i = 0; i< _tasks.size();i++) _tasks[i]->doTask(_id, data); }
+
+      void doTasks(std::vector<String> &payload) { for(auto i = 0; i< _tasks.size();i++) { _tasks[i]->doMqttTask(_id, payload); } }
+
+      void doTasks(JsonObject& iroot, JsonObject& root) { for(auto i = 0; i< _tasks.size();i++) _tasks[i]->doWStask(_id, iroot, root); }
 
    public:
       int _id;
       int _type;
       std::vector<EspTask *> _tasks;
+};
+
+struct EspEvent
+{
+   EspEventPtr *evt;
+   void *data;
+   EspEvent(EspEventPtr *e, void *d = nullptr): evt(e),data(d) {}
 };
